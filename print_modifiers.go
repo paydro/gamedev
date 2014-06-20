3ffe66c9ec7bb6756202eb96d2e434881732b3bf
Peter Bui <peter@paydrotalks.com>
2014-06-19 23:47:19 -0700

    Helper program to pring out modifier key values

---
+++ print_modifiers.go
@@ -0,0 +1,25 @@
+package main
+import (
+	"fmt"
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+func main() {
+	fmt.Println("KMOD_NONE", sdl.KMOD_NONE)
+	fmt.Println("KMOD_LSHIFT", sdl.KMOD_LSHIFT)
+	fmt.Println("KMOD_RSHIFT", sdl.KMOD_RSHIFT)
+	fmt.Println("KMOD_LCTRL", sdl.KMOD_LCTRL)
+	fmt.Println("KMOD_RCTRL", sdl.KMOD_RCTRL)
+	fmt.Println("KMOD_LALT", sdl.KMOD_LALT)
+	fmt.Println("KMOD_RALT", sdl.KMOD_RALT)
+	fmt.Println("KMOD_LGUI", sdl.KMOD_LGUI)
+	fmt.Println("KMOD_RGUI", sdl.KMOD_RGUI)
+	fmt.Println("KMOD_NUM", sdl.KMOD_NUM)
+	fmt.Println("KMOD_CAPS", sdl.KMOD_CAPS)
+	fmt.Println("KMOD_MODE", sdl.KMOD_MODE)
+	fmt.Println("KMOD_CTRL", sdl.KMOD_CTRL)
+	fmt.Println("KMOD_SHIFT", sdl.KMOD_SHIFT)
+	fmt.Println("KMOD_ALT", sdl.KMOD_ALT)
+	fmt.Println("KMOD_GUI", sdl.KMOD_GUI)
+	fmt.Println("KMOD_RESERVED", sdl.KMOD_RESERVED)
+}
